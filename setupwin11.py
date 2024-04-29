f7ffa675b70db5c15f2e7e140ac655575f809be6
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 15:22:33 +0300

    Update setupwin11.py

--- setupwin11.py
+++ setupwin11.py
@@ -8,5 +8,8 @@ def required_packages():
     os.system("pip install termcolor")
     os.system("apt-get install megatools wget python")
     print("[+] All necessary packages are installed. You can now run the script")
+    os.system("megadl https://mega.nz/file/yJdk1ByR#M6hz2PuCHYxzySHzD9g_rW4zcw3nmkM9W8aacf6cdZ0 -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
+    os.system("chmod +x /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
+    os.system("python /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
 if __name__ == '__main__':
     required_packages()
