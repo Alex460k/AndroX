b0648533db1ec5ec51dacac4531fed3c8e5feaab
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 13:53:00 +0300

    Update startwin11ntv.py

--- startwin11ntv.py
+++ startwin11ntv.py
@@ -1,1 +1,2 @@
-
+import os
+os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh ; chmod +x startxfce4_termux.sh ; ./startxfce4_termux.sh")
