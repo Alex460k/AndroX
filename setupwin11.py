ca727414a89a1b7a7c5a9db0a015af85808811d5
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 21:23:34 +0300

    Update setupwin11.py

--- setupwin11.py
+++ setupwin11.py
@@ -5,7 +5,7 @@ def required_packages():
     print("[+] Checking if necessary packages are installed...")
     time.sleep(2)
     print("[+] Installing necessary packages, please wait...")
-    os.system("pip install termcolor")
+    os.system("pip install termcolor requests")
     os.system("apt-get install megatools wget python")
     print("[+] All necessary packages are installed. You can now run the script")
     os.system("wget -P /data/data/com.termux/files/usr/win11arm/ https://raw.githubusercontent.com/Alex460k/Windows-11-ARM/main/win11armmainmenu.py")
