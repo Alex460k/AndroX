ef5b5d8773f2bf962b9ca8b7a280404fbe974a50
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 16:21:57 +0300

    Update startwin11ntv.py

--- startwin11ntv.py
+++ startwin11ntv.py
@@ -1,2 +1,8 @@
 import os
-os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh ; chmod +x startxfce4_termux.sh ; ./startxfce4_termux.sh")
+
+if not os.path.exists("/data/data/com.termux/files/home/startxfce4_termux.sh"):
+    os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh")
+    os.system("chmod +x startxfce4_termux.sh")
+    os.system("./startxfce4_termux.sh")
+else:
+    os.system("./startxfce4_termux.sh")
