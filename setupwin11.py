468c2228fdbf12578466a1b88ff19e6fef1dd427
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 18:12:17 +0300

    Update setupwin11.py

--- setupwin11.py
+++ setupwin11.py
@@ -10,8 +10,8 @@ def required_packages():
     print("[+] All necessary packages are installed. You can now run the script")
     os.system("megadl -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py https://mega.nz/file/Gc0X2QKJ#YCCkJx2-2kok2h1e7NEPCwcUkW2QesmymjAICA5Xb9I ; cd Windows-11-ARM ; mv win11armmainmenu.py /data/data/com.termux/files/usr/win11arm/")
     os.system("chmod +x /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
-    os.system("megadl -f -p $PREFIX/bin https://mega.nz/file/mRU12SZI#309lDCcI0eiwC0MSGxL4n_BPvawPwx_rC6i4NAjZ49o ; cd Windows-11-ARM ; mv win11arm $PREFIX/bin")
-    os.system("chmod +x $PREFIX/bin/win11arm")
+    os.system("megadl -f -p $PREFIX/bin https://mega.nz/file/mRU12SZI#309lDCcI0eiwC0MSGxL4n_BPvawPwx_rC6i4NAjZ49o ; cd Windows-11-ARM ; mv win11arm /data/data/com.termux/files/usr/bin/")
+    os.system("chmod +x /data/data/com.termux/files/usr/bin/win11arm")
     print("Type 'win11arm' in termux to start the script. ENJOY!")
 if __name__ == '__main__':
     required_packages()
