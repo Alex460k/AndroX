60fc08d7edd2118fb28182feb20030f826ce734b
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 20:57:54 +0300

    Update startwin11arm.py

--- startwin11arm.py
+++ startwin11arm.py
@@ -1,8 +1,8 @@
 import os
 
-if not os.path.exists("/data/data/com.termux/files/home/startxfce4_nethunter.sh"):
-    os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/proot_kali/startxfce4_nethunter.sh")
-    os.system("chmod +x startxfce4_nethunter.sh")
-    os.system("./startxfce4_nethunter.sh")
+if not os.path.exists("/data/data/com.termux/files/home/startxfce4\_nethunter.sh"):
+    os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/proot\_kali/startxfce4\_nethunter.sh")
+    os.system("chmod +x startxfce4\_nethunter.sh")
+    os.system("./startxfce4\_nethunter.sh")
 else:
-    os.system("./startxfce4_nethunter.sh")
+    os.system("./startxfce4\_nethunter.sh")
