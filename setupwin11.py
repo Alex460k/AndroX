34b92aafa2cd2fb0d930ea969b75807f92262ddd
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 18:21:55 +0300

    Update setupwin11.py

--- setupwin11.py
+++ setupwin11.py
@@ -8,7 +8,7 @@ def required_packages():
     os.system("pip install termcolor")
     os.system("apt-get install megatools wget python")
     print("[+] All necessary packages are installed. You can now run the script")
-    os.system("megadl -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py https://mega.nz/file/Gc0X2QKJ#YCCkJx2-2kok2h1e7NEPCwcUkW2QesmymjAICA5Xb9I ; cd Windows-11-ARM ; mv win11armmainmenu.py /data/data/com.termux/files/usr/win11arm/")
+    os.system("megadl -f -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py https://mega.nz/file/uJdghAoC#GmoVr4B-BDx4EISlawZmGXShxuqQfG3y0Ayl8Z_4KSE ; cd Windows-11-ARM ; mv win11armmainmenu.py /data/data/com.termux/files/usr/win11arm/")
     os.system("chmod +x /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
     os.system("megadl -f -p $PREFIX/bin https://mega.nz/file/mRU12SZI#309lDCcI0eiwC0MSGxL4n_BPvawPwx_rC6i4NAjZ49o ; cd Windows-11-ARM ; mv win11arm /data/data/com.termux/files/usr/bin/")
     os.system("chmod +x /data/data/com.termux/files/usr/bin/win11arm")
