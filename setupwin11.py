13d26dd63496b68b8fdca9a5ae837515a125adfb
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 17:35:45 +0300

    Update setupwin11.py

--- setupwin11.py
+++ setupwin11.py
@@ -8,9 +8,9 @@ def required_packages():
     os.system("pip install termcolor")
     os.system("apt-get install megatools wget python")
     print("[+] All necessary packages are installed. You can now run the script")
-    os.system("megadl https://mega.nz/file/jJ02AQAB#ckyS-XSmxR7nERothNKfZNo8wpCzKs_QcZWQwBdFAuM -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
+    os.system("megadl -p /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py https://mega.nz/file/jJ02AQAB#ckyS-XSmxR7nERothNKfZNo8wpCzKs_QcZWQwBdFAuM")
     os.system("chmod +x /data/data/com.termux/files/usr/win11arm/win11armmainmenu.py")
-    os.system("megadl https://mega.nz/file/mRU12SZI#309lDCcI0eiwC0MSGxL4n_BPvawPwx_rC6i4NAjZ49o -p $PREFIX/bin")
+    os.system("megadl -p $PREFIX/bin https://mega.nz/file/mRU12SZI#309lDCcI0eiwC0MSGxL4n_BPvawPwx_rC6i4NAjZ49o")
     os.system("chmod +x $PREFIX/bin/win11arm")
     print("Type 'win11arm' in termux to start the script. ENJOY!")
 if __name__ == '__main__':
