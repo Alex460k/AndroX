2fcfad1ae7dc97eab27535c6b8dc0446220e15a4
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 13:52:43 +0300

    Update startwin11arm.py

---
+++ startwin11arm.py
@@ -0,0 +1,2 @@
+import os
+os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/proot_kali/startxfce4_nethunter.sh ; chmod +x startxfce4_nethunter.sh ; ./startxfce4_nethunter.sh")
