644426b4b96cfdb0aae62ba60984811c97b658e1
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-29 13:56:58 +0300

    Add files via upload

---
+++ setupwin11.py
@@ -0,0 +1,11 @@
+import os
+def required_packages():
+    os.system("clear")
+    print("[+] Checking if necessary packages are installed...")
+    time.sleep(2)
+    print("[+] Installing necessary packages, please wait...")
+    os.system("pip install termcolor")
+    os.system("apt-get install megatools wget python")
+    print("[+] All necessary packages are installed. You can now run the script")
+if __name__ == '__main__':
+    required_packages()
