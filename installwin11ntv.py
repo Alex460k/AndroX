a6d9afb590fdf317199d9982a6db1f497c647f0e
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-30 11:08:34 +0300

    Update installwin11ntv.py

--- installwin11ntv.py
+++ installwin11ntv.py
@@ -1,11 +1,7 @@
 import os
 import time
-def linux():
-    os.system("clear")
-    print("Also need Internet!")
-    time.sleep(2)
-    os.system("termux-setup-storage ; pkg install x11-repo ; pkg install termux-x11-nightly ; pkg install pulseaudio ; pkg install xfce4 xfce4-goodies ; pkg install tur-repo ; pkg update ; pkg install chromium code-oss ; wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/mobox_run.sh ; chmod +x mobox_run.sh")
-    print("Instalation Succesfully, now you can start Windows 11 ARM Native!")
-
-if __name__ == '__main__':
-    linux()
+os.system("clear")
+print("Also need Internet!")
+time.sleep(2)
+os.system("termux-setup-storage ; pkg install x11-repo ; pkg install termux-x11-nightly ; pkg install pulseaudio ; pkg install xfce4 xfce4-goodies ; pkg install tur-repo ; pkg update ; pkg install chromium code-oss ; wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/mobox_run.sh ; chmod +x mobox_run.sh")
+print("Instalation Succesfully, now you can start Windows 11 ARM Native!")
