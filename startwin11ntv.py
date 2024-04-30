f4b10b67bdaa986ba5dc2f68a1a9e9012107cb1b
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-04-30 07:01:18 +0300

    Update startwin11ntv.py

--- startwin11ntv.py
+++ startwin11ntv.py
@@ -1,8 +1,8 @@
-import os
+import subprocess
 
 if not os.path.exists("/data/data/com.termux/files/home/startxfce4_termux.sh"):
-    os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh")
-    os.system("chmod +x startxfce4_termux.sh")
-    os.system("./startxfce4_termux.sh")
+    subprocess.run(["wget", "https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh"], shell=True)
+    subprocess.run(["chmod", "+x", "startxfce4_termux.sh"], shell=True)
+    subprocess.run(["./startxfce4_termux.sh"], shell=True)
 else:
-    os.system("./startxfce4_termux.sh")
+    subprocess.run(["./startxfce4_termux.sh"], shell=True)
