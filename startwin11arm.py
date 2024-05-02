43b928fc84b0697f92aa79fc12a5de60abbd4351
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-05-02 21:34:28 +0300

    Delete startwin11arm.py

--- startwin11arm.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/proot_kali/startxfce4_nethunter.sh")
-os.system("chmod +x startxfce4_nethunter.sh")
-os.system("./startxfce4_nethunter.sh")
