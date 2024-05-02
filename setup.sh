0483ba157a131db7507cae82199bc26aa3028309
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-05-02 21:45:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-apt update && apt upgrade && pkg install python && git clone https://github.com/Alex460k/Windows-11-ARM.git && cd Windows-11-ARM && bash start.sh
+apt update && apt upgrade && pkg install python git && git clone https://github.com/Alex460k/Project.git && cd Project && bash start.sh
