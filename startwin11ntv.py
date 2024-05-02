608de8a3c10941989e1f3f338b87b88ff658c0cb
DeathGhost <123093774+Alex460k@users.noreply.github.com>
2024-05-02 21:33:49 +0300

    Delete startwin11ntv.py

--- startwin11ntv.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh")
-os.system("chmod +x startxfce4_termux.sh")
-os.system("./startxfce4_termux.sh")
